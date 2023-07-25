be7be0e847450f582fb4778843387888f4e10f45
Sumit0397 <123931330+Sumit0397@users.noreply.github.com>
2023-07-25 11:58:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+
+export default function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
